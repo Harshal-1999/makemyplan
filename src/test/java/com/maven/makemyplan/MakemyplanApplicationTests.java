97f8cb8432b060bed6ba94b1736eb38d9e5e2f38
Harshal-1999 <harshalbhosale24@gmail.com>
2020-04-15 13:47:04 +0530

    coderefactor

---
+++ src/test/java/com/maven/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.makemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
